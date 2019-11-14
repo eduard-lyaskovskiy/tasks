99ece7b0c83d62530997a3bee8ce74f7a377308b
Eduard <leskov778@gmail.com>
2019-11-14 21:50:09 +0200

    task 1 commit

---
+++ task_1/script.js
@@ -0,0 +1,23 @@
+"use strict";
+// 2
+let money = prompt("Ваш бюджет на месяц?");
+let time = prompt("Введите дату в формате YYYY-MM-DD", "YYYY-MM-DD");
+//4
+let oblExp = prompt("Введите обязательную статью расходов в этом месяце"); 
+let totalExp = prompt("Во сколько обойдется?");
+
+let expenses = new Object ({
+    oblExp : totalExp
+});
+console.log(expenses);
+//3
+let appData = new Object ({
+    "budget" : money,
+    "timeData" : time,
+    "obligatoryExpenses" : oblExp,
+    "optionalExpenses" : "",
+    "income" : [],
+    "savings" : false
+});
+console.log('%c appData', 'color: orange; font-weight: bold;' );
+console.log({ budget, timeData, obligatoryExpenses, optionalExpenses, income, savings});
