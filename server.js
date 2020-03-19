b6ab7b8e304c0c85f89b3508091538135675728d
Eduard <leskov778@gmail.com>
2020-03-20 00:31:03 +0200

    add task 13

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+const bodyParser = require('body-parser');
+const workDir = `${path.join(__dirname)}/task_13/`;
+
+app.use(express.static(workDir));
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.json({
+    type: ['application/json', 'text/plain']
+  }))
+
+app.get('/', function(req, res) {
+    res.sendFile(`${workDir}index.html`)
+})
+
+app.post('/', (req, res) => {
+    res.send(req.body);
+})
+
+app.listen(port, () => {
+    console.log(`Server work at port: ${port}`)
+})
