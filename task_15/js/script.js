5a5beaafe2f6703b4a45ff64db31abedd2ca1974
Eduard <leskov778@gmail.com>
2020-03-30 21:26:13 +0300

    added task 16, calc, fix bug

--- task_15/js/script.js
+++ task_15/js/script.js
@@ -235,5 +235,48 @@ document.addEventListener("DOMContentLoaded", function() {
                 currentSlides(i);
             }
         }
+    });
+
+    //calculator
+
+    let persons    = document.querySelectorAll('.counter-block-input')[0],
+        restDays   = document.querySelectorAll('.counter-block-input')[1],
+        place      = document.getElementById('select'),
+        totalValue = document.getElementById('total'),
+        personsSum = 0,
+        daysSum    = 0,
+        total      = 0;
+
+    totalValue.innerHTML = total;
+    
+    persons.addEventListener('change', function() {
+        personsSum = +this.value;
+        total = (personsSum + daysSum) * 1500;
+        
+        if (!restDays.value || !persons.value) {
+            totalValue.innerHTML = 0;
+        } else {
+            totalValue.innerHTML = total
+        }
+    })
+
+    restDays.addEventListener('change', function() {
+        daysSum = +this.value;
+        total = (personsSum + daysSum) * 1500;
+        
+        if (!persons.value || !restDays.value) {
+            totalValue.innerHTML = 0;
+        } else {    
+            totalValue.innerHTML = total
+        }
+    })
+
+    place.addEventListener('change', function() {
+        if(!persons.value || !restDays.value) {
+            totalValue.innerHTML = 0;
+        } else {
+            let localTotal = total;
+            totalValue.innerHTML = localTotal * this.options[this.selectedIndex].value
+        }
     })
 });
